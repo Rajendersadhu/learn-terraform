87068c8108e997084bb8070d72c2e1dff2e828c5
Rajendersadhu <Rajender2sr@gmail.com>
2023-07-16 12:21:36 +0530

    playbook

---
+++ 02-outputs/output.tf
@@ -0,0 +1,17 @@
+
+output "sample" {
+  value = "Hello World"
+}
+
+# any string in terraform need to be quoted with double quotes only.
+# single quotes are not supported
+
+output "sample" {
+  value = 1000
+}
+
+# Numbers and booleans (true, false)
+
+
+
+
